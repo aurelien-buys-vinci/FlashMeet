3c4e50e82d0543321fb267112e450b8a2a96d496
buys <aurelien.buys@student.vinci.be>
2025-10-01 19:05:51 -0400

    Initialize FlashMeet project with Maven and Spring Boot setup

---
+++ FlashMeet/src/main/java/be/aurelien/buys/flashmeet/FlashMeetApplication.java
@@ -0,0 +1,13 @@
+package be.aurelien.buys.flashmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashMeetApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlashMeetApplication.class, args);
+  }
+
+}
---
+++ FlashMeet/src/test/java/be/aurelien/buys/flashmeet/FlashMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package be.aurelien.buys.flashmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashMeetApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
